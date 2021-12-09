bd078fa1bec64610e41d3c20ee4bee173c2be810
Nazmul <nhshishir914@gmail.com>
2021-12-09 23:29:21 +0600

    initail commit 12/9/21

---
+++ src/test/java/com/inetbanking/testCases/TC_AddCustomerTest_003.java
@@ -0,0 +1,99 @@
+package com.inetbanking.testCases;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.inetbanking.pageObjects.AddCustomerPage;
+import com.inetbanking.pageObjects.LoginPage;
+
+public class TC_AddCustomerTest_003 extends BaseClass
+{
+
+	@Test
+	public void addNewCustomer() throws InterruptedException, IOException
+	{
+		LoginPage lp=new LoginPage(driver);
+		lp.setUserName(username);
+		logger.info("User name is provided");
+		lp.setPassword(password);
+		logger.info("Passsword is provided");
+		lp.clickSubmit();
+		
+		Thread.sleep(3000);
+
+		
+		if(isAlertPresent()==true)
+		{
+			driver.switchTo().alert().accept();//close alert
+			driver.switchTo().defaultContent();
+			//Assert.assertTrue(false);
+			logger.warn("Login failed");
+		}
+		else
+		{
+			Assert.assertTrue(true);
+			logger.info("Login passed");
+			//lp.clickLogout();
+			Thread.sleep(3000);
+			//driver.switchTo().alert().accept();//close logout alert
+			//driver.switchTo().defaultContent();
+			
+		}
+		
+		AddCustomerPage addcust=new AddCustomerPage(driver);
+		
+		addcust.clickAddNewCustomer();
+		
+		logger.info("providing customer details....");
+		
+		
+		addcust.custName("Pavan");
+		addcust.custgender("male");
+		addcust.custdob("10","15","1985");
+		Thread.sleep(5000);
+		addcust.custaddress("INDIA");
+		addcust.custcity("HYD");
+		addcust.custstate("AP");
+		addcust.custpinno("5000074");
+		addcust.custtelephoneno("987890091");
+		
+		String email=randomestring()+"@gmail.com";
+		addcust.custemailid(email);
+		addcust.custpassword("abcdef");
+		addcust.custsubmit();
+		
+		Thread.sleep(3000);
+		
+		logger.info("validation started....");
+		
+		boolean res=driver.getPageSource().contains("Customer Registered Successfully!!!");
+		
+		if(res==true)
+		{
+			Assert.assertTrue(true);
+			logger.info("test case passed....");
+			
+		}
+		else
+		{
+			logger.info("test case failed....");
+			captureScreen(driver,"addNewCustomer");
+			Assert.assertTrue(false);
+		}
+			
+	}
+
+	private boolean isAlertPresent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+//	private boolean isAlertPresent() {
+//		// TODO Auto-generated method stub
+//		return false;
+//	}
+	
+	
+}
